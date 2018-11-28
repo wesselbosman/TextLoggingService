481cadd5cf23e0142e94fa4fe3580a517ede65fc
Wessel Bosman <wessel.bosman@hotmail.com>
2018-11-28 14:59:27 +0000

    Create TextLogReaderService

---
+++ TextLoggingService.Core.UnitTests/TextLogReaderServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+using Shouldly;
+using System.Threading.Tasks;
+using TextLoggingService.Core.Logging;
+using TextLoggingService.Core.StorageProviders;
+using Xunit;
+
+namespace TextLoggingService.Core.UnitTests
+{
+    public class TextLogReaderServiceTests
+    {
+        private readonly ILogStorageProvider _logStorageProvider;
+
+        public TextLogReaderServiceTests()
+        {
+            _logStorageProvider = Substitute.For<ILogStorageProvider>();
+        }
+
+        [Fact]
+        public async Task Read_WhenStorageProviderReturnsLogfile_ShouldReturnLogfile()
+        {
+            // Arrange
+            var expectedLog = "Doth God exact day labour, light denied?";
+            _logStorageProvider.Read().Returns(expectedLog);
+
+            var logReaderService = new TextLogReaderService(_logStorageProvider);
+
+            // Act
+            var readResult = await logReaderService.Read();
+
+            // Assert
+            readResult.ShouldBe(expectedLog);
+        }
+    }
+}
---
+++ TextLoggingService.Core/Logging/TextLogReaderService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using TextLoggingService.Core.StorageProviders;
+
+namespace TextLoggingService.Core.Logging
+{
+    public class TextLogReaderService : ILogReaderService
+    {
+        private readonly ILogStorageProvider _logStorageProvider;
+
+        public TextLogReaderService(ILogStorageProvider logStorageProvider)
+        {
+            _logStorageProvider = logStorageProvider;
+        }
+
+        public async Task<string> Read()
+        {
+            return await _logStorageProvider.Read();
+        }
+    }
+}
