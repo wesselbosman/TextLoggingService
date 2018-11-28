71ce209af6f28865b12641bb15ff4fd396e8c56e
Wessel Bosman <wessel.bosman@hotmail.com>
2018-11-28 15:02:42 +0000

    Add Logging Controller

---
+++ TextLoggingService/Controllers/LoggingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TextLoggingService.Core.Logging;
+
+namespace TextLoggingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoggingController : ControllerBase
+    {
+        private readonly ILogWriterService _logWriterService;
+        private readonly ILogReaderService _logReaderService;
+
+        public LoggingController(ILogWriterService logWriterService, ILogReaderService logReaderService)
+        {
+            _logWriterService = logWriterService;
+            _logReaderService = logReaderService;
+        }
+
+        [Route("write"), HttpPost]
+        public async Task<IActionResult> Write([FromBody] LogModel logModel)
+        {
+            await _logWriterService.AppendLogMessage(logModel);
+            return Accepted();
+        }
+
+        [Route("read"), HttpGet]
+        public async Task<IActionResult> Read()
+        {
+            var entireLog = await _logReaderService.Read();
+            return Ok(entireLog);
+        }
+    }
+}
